d02967d3cf317127e9653af0370964f472ff322e
winwinhun <dksxlahsl@naver.com>
2026-02-27 15:22:58 +0900

    rename : member -> user

---
+++ gamyeon-user-service/src/main/java/com/gamyeon/userservice/GamyeonUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gamyeon.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamyeonUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamyeonUserServiceApplication.class, args);
+    }
+
+}
