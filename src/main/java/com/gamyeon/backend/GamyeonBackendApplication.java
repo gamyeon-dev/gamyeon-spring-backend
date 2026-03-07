d81808f396f6a36ccc7c619c9cbc4244dcacd2e8
hojun-lang <milky9925@gmail.com>
2026-03-08 02:03:16 +0900

    firts commit

---
+++ src/main/java/com/gamyeon/backend/GamyeonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gamyeon.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamyeonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamyeonBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gamyeon/backend/GamyeonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamyeon.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamyeonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
