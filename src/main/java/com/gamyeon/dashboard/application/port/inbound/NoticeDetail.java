659e0068400736840ab650bcb5af4171cc155d16
winwinhun <dksxlahsl@naver.com>
2026-03-11 09:27:35 +0900

    feat(dashboard): 공지사항 내 이미지 첨부 기능 추가

--- src/main/java/com/gamyeon/dashboard/application/port/inbound/NoticeDetail.java
+++ src/main/java/com/gamyeon/dashboard/application/port/inbound/NoticeDetail.java
@@ -3,14 +3,19 @@ package com.gamyeon.dashboard.application.port.inbound;
 import com.gamyeon.dashboard.domain.Notice;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-public record NoticeDetail(Long id, String title, String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
+public record NoticeDetail(Long id, String title, String content, List<String> imageUrls, LocalDateTime createdAt, LocalDateTime updatedAt) {
 
     public static NoticeDetail from(Notice notice) {
+        List<String> imageUrls = notice.getImages().stream()
+                .map(image -> image.getImageUrl())
+                .toList();
         return new NoticeDetail(
                 notice.getId(),
                 notice.getTitle(),
                 notice.getContent(),
+                imageUrls,
                 notice.getCreatedAt(),
                 notice.getUpdatedAt()
         );
