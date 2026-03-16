a29d4564afa577a8199688476ef4d827f5877193
betterb2etr <b2etr@naver.com>
2026-03-17 04:20:26 +0900

    fix: spotles

--- src/main/java/com/gamyeon/intv/infrastructure/JpaIntvRepository.java
+++ src/main/java/com/gamyeon/intv/infrastructure/JpaIntvRepository.java
@@ -3,5 +3,4 @@ package com.gamyeon.intv.infrastructure;
 import com.gamyeon.intv.domain.Intv;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaIntvRepository extends JpaRepository<Intv, Long> {
-}
+public interface JpaIntvRepository extends JpaRepository<Intv, Long> {}
