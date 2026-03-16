e1a42865c7a890302f8aa5e810d700e4bd70ce44
JaeWoong Eom <zzang6535@gmail.com>
2026-03-16 18:55:54 +0900

    fix: consul kv설정 확인
    consul kv에서 값을 불러오도록 설정 확인 위한 컨트롤러 설정

---
+++ src/main/java/com/gamyeon/test/ConfigCheckController.java
@@ -0,0 +1,17 @@
+package com.gamyeon.test;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigCheckController {
+
+    @Value("${storage.s3.bucket}")
+    private String secret;
+
+    @GetMapping("/check-config")
+    public String check() {
+        return "Loaded secret: " + secret;
+    }
+}
