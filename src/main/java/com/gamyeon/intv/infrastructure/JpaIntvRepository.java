f5d87e53d1b29152978f9d9e31986ef00708f65b
betterb2etr <b2etr@naver.com>
2026-03-17 04:19:21 +0900

    fix: infrastructuree

--- src/main/java/com/gamyeon/intv/infrastructure/JpaIntvRepository.java
+++ src/main/java/com/gamyeon/intv/infrastructure/JpaIntvRepository.java
@@ -1,4 +1,4 @@
-package com.gamyeon.intv.infrastructuree;
+package com.gamyeon.intv.infrastructure;
 
 import com.gamyeon.intv.domain.Intv;
 import org.springframework.data.jpa.repository.JpaRepository;
