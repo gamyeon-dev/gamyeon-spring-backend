603e830f4c0979ca6f0a35e888eba163b492c5d6
hojun-lang <milky9925@gmail.com>
2026-03-17 19:05:00 +0900

    fix(answer): spotless

--- src/main/java/com/gamyeon/question/adaptor/out/PythonQuestionGenerationFeignClient.java
+++ src/main/java/com/gamyeon/question/adaptor/out/PythonQuestionGenerationFeignClient.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(name = "pythonQuestionGenerationClient", url = "{ai.server.url}")
 public interface PythonQuestionGenerationFeignClient {
 
-    @PostMapping("/internal/v1/questions/generate")
-    void requestGeneration(@RequestBody AiQuestionGenerationRequest request);
+  @PostMapping("/internal/v1/questions/generate")
+  void requestGeneration(@RequestBody AiQuestionGenerationRequest request);
 }
