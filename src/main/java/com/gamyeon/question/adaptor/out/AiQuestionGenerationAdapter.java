09f104887f5b3fb99c68befdb619ce78adf3e89d
hojun-lang <milky9925@gmail.com>
2026-03-17 10:36:14 +0900

    fix(question-set) : openFeignClient 적용

--- src/main/java/com/gamyeon/question/adaptor/out/AiQuestionGenerationAdapter.java
+++ src/main/java/com/gamyeon/question/adaptor/out/AiQuestionGenerationAdapter.java
@@ -2,17 +2,19 @@ package com.gamyeon.question.adaptor.out;
 
 import com.gamyeon.question.application.port.out.GenerateCustomQuestionPort;
 import com.gamyeon.question.application.port.out.PreparationForQuestionGeneration;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class AiQuestionGenerationAdapter implements GenerateCustomQuestionPort {
+
+  private final PythonQuestionGenerationFeignClient pythonQuestionGenerationFeignClient;
 
   @Override
   public void request(PreparationForQuestionGeneration preparation, String callbackUrl) {
     AiQuestionGenerationRequest request =
         AiQuestionGenerationRequest.from(preparation, callbackUrl);
-
-    // TODO : 여기서 AI API 호출!!
-    // @PythonApi(request)(/internal/v1/questions/generate)
+    pythonQuestionGenerationFeignClient.requestGeneration(request);
   }
 }
---
+++ src/main/java/com/gamyeon/question/adaptor/out/PythonQuestionGenerationFeignClient.java
@@ -0,0 +1,12 @@
+package com.gamyeon.question.adaptor.out;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "pythonQuestionGenerationClient", url = "/internal/v1/questions/generate")
+public interface PythonQuestionGenerationFeignClient {
+
+  @PostMapping("/internal/v1/questions/generate")
+  void requestGeneration(@RequestBody AiQuestionGenerationRequest request);
+}
